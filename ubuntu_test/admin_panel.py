43fca5e9efa5227aaf83c36b944667ad8c181b23
Miraku-dev <85035112+Miraku-dev@users.noreply.github.com>
2021-06-29 00:59:54 +0300

    Update admin_panel.py

--- ubuntu_test/admin_panel.py
+++ ubuntu_test/admin_panel.py
@@ -5,20 +5,20 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import admin_id
-from load_all import dp, _, bot
+from load_all import dp, bot
 from states import NewItem, Mailing
 from database import Item, User
 
 
 @dp.message_handler(user_id=admin_id, commands=["cancel"], state=NewItem)
 async def cancel(message: types.Message, state: FSMContext):
-    await message.answer(_("Вы отменили создание товара"))
+    await message.answer("Вы отменили создание товара")
     await state.reset_state()
 
 
 @dp.message_handler(user_id=admin_id, commands=["add_item"])
 async def add_item(message: types.Message):
-    await message.answer(_("Введите название товара или нажмите /cancel"))
+    await message.answer("Введите название товара или нажмите /cancel")
     await NewItem.Name.set()
 
 
@@ -28,8 +28,8 @@ async def enter_name(message: types.Message, state: FSMContext):
     item = Item()
     item.name = name
 
-    await message.answer(_("Название: {name}"
-                           "\nПришлите мне фотографию товара (не документ) или нажмите /cancel").format(name=name))
+    await message.answer("Название: {name}"
+                           "\nПришлите мне фотографию товара (не документ) или нажмите /cancel").format(name=name)
 
     await NewItem.Photo.set()
     await state.update_data(item=item)
@@ -44,7 +44,7 @@ async def add_photo(message: types.Message, state: FSMContext):
 
     await message.answer_photo(
         photo=photo,
-        caption=_("Название: {name}"
+        caption=("Название: {name}"
                   "\nПришлите мне цену товара в копейках или нажмите /cancel").format(name=item.name))
 
     await NewItem.Price.set()
@@ -58,18 +58,18 @@ async def enter_price(message: types.Message, state: FSMContext):
     try:
         price = int(message.text)
     except ValueError:
-        await message.answer(_("Неверное значение, введите число"))
+        await message.answer("Неверное значение, введите число")
         return
 
     item.price = price
     markup = InlineKeyboardMarkup(
         inline_keyboard=
         [
-            [InlineKeyboardButton(text=_("Да"), callback_data="confirm")],
-            [InlineKeyboardButton(text=_("Ввести заново"), callback_data="change")],
+            [InlineKeyboardButton(text=("Да"), callback_data="confirm")],
+            [InlineKeyboardButton(text=("Ввести заново"), callback_data="change")],
         ]
     )
-    await message.answer(_("Цена: {price:,}\n"
+    await message.answer(("Цена: {price:,}\n"
                            "Подтверждаете? Нажмите /cancel чтобы отменить").format(price=price / 100),
                          reply_markup=markup)
     await state.update_data(item=item)
@@ -79,7 +79,7 @@ async def enter_price(message: types.Message, state: FSMContext):
 @dp.callback_query_handler(user_id=admin_id, text_contains="change", state=NewItem.Confirm)
 async def enter_price(call: types.CallbackQuery):
     await call.message.edit_reply_markup()
-    await call.message.answer(_("Введите заново цену товара в копейках"))
+    await call.message.answer("Введите заново цену товара в копейках")
     await NewItem.Price.set()
 
 
@@ -89,14 +89,14 @@ async def enter_price(call: types.CallbackQuery, state: FSMContext):
     data = await state.get_data()
     item: Item = data.get("item")
     await item.create()
-    await call.message.answer(_("Товар удачно создан."))
+    await call.message.answer("Товар удачно создан.")
     await state.reset_state()
 
 
 # Фича для рассылки по юзерам (учитывая их язык)
 @dp.message_handler(user_id=admin_id, commands=["tell_everyone"])
 async def mailing(message: types.Message):
-    await message.answer(_("Пришлите текст рассылки"))
+    await message.answer("Пришлите текст рассылки")
     await Mailing.Text.set()
 
 
@@ -112,7 +112,7 @@ async def mailing(message: types.Message, state: FSMContext):
             [InlineKeyboardButton(text="Україньска", callback_data="uk")],
         ]
     )
-    await message.answer(_("Пользователям на каком языке разослать это сообщение?\n\n"
+    await message.answer(("Пользователям на каком языке разослать это сообщение?\n\n"
                            "Текст:\n"
                            "{text}").format(text=text),
                          reply_markup=markup)
