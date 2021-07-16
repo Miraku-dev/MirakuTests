dbb3a9625e266a7944114d8c225256637012f329
Miraku-dev <85035112+Miraku-dev@users.noreply.github.com>
2021-07-16 17:03:48 +0300

    Add files via upload

--- ubuntu/admin_panel.py
+++ ubuntu/admin_panel.py
@@ -467,7 +467,7 @@ async def delete_item(call: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     id = data.get("id")
     await Item.delete.where(Item.id == id).gino.status()
-    item_id = await Item.query.where(Item.id < id).gino.all()
+    item_id = await Item.query.where(Item.id < id).gino.scalar()
     item_id1=(item_id[-1:]+item_id[:-1])
     await Item.update(item_id == item_id1).apply()
     markup = InlineKeyboardMarkup(
