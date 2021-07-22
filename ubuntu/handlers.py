47b8e04be9d778b68ff2d8c502cf01e67a550781
Miraku-dev <85035112+Miraku-dev@users.noreply.github.com>
2021-07-22 19:30:06 +0300

    Add files via upload

--- ubuntu/handlers.py
+++ ubuntu/handlers.py
@@ -356,10 +356,7 @@ async def show_hats(call: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     category = data.get("category")
     id = data.get("id")
-
-    for message_id in range((call.message.message_id-2),call.message.message_id):
-        bot.delete_message(call.message.chat.id, message_id)
-    
+    bot.delete_message(-2)
     next_id = data.get("next_id")
     all_items = await database.Item.query.where(database.Item.category == category).where(
         database.Item.id != id).where(database.Item.id != next_id).limit(2).gino.all()
