6243dd19dd45b74f570bd707d055f6bfa2eccd52
Miraku-dev <85035112+Miraku-dev@users.noreply.github.com>
2021-08-19 17:09:23 +0300

    Add files via upload

--- ubuntu/handlers.py
+++ ubuntu/handlers.py
@@ -114,9 +114,7 @@ async def show_hats(call: CallbackQuery, state: FSMContext):
                 ],
             ]
         )
-        # Отправляем фотку товара с подписью и кнопкой "купить"
-        await call.message.answer_media_group(
-            media=item.media)
+        await call.message.answer_media_group(media=item.media)
         await call.message.edit_reply_markup(reply_markup=markup)
         await state.update_data(id=id)
         # Между сообщениями делаем небольшую задержку, чтобы не упереться в лимиты
