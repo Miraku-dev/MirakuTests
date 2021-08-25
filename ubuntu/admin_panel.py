0ae994bc9e7d94704e2466e67cc15a950bc52862
Miraku-dev <85035112+Miraku-dev@users.noreply.github.com>
2021-08-26 01:16:01 +0300

    Add files via upload

--- ubuntu/admin_panel.py
+++ ubuntu/admin_panel.py
@@ -290,13 +290,14 @@ async def add_photo(message: types.Message, state: FSMContext):
                                 else:
                                     if item.photo_9 == None:
                                         item.photo_9 = photo
-    if item.photo_9 != None:
-        await message.answer("Достигнуто максимальное количество медиафайлов.", reply_markup=button)
+                                    else:
+                                        if item.photo_9 != None:
+                                            await message.answer("Достигнуто максимальное количество медиафайлов.", reply_markup=button)
         
-
-    await message.answer_photo(photo=photo, caption="Фото добавлено.\n"
-            "Название: {name}"
-            '\nПришлите ещё одно фото или видео или нажмите "Готово"'.format(name=item.name), reply_markup=button)
+    if item.photo_9 == None:
+        await message.answer_photo(photo=photo, caption="Фото добавлено.\n"
+                "Название: {name}"
+                '\nПришлите ещё одно фото или видео или нажмите "Готово"'.format(name=item.name), reply_markup=button)
     await state.update_data(item=item)
     await NewItem.Photo.set()
 
@@ -340,8 +341,9 @@ async def add_video(message: types.Message, state: FSMContext):
                                 else:
                                     if item.photo_9 == None:
                                         item.photo_9 = video
-    if item.photo_9 != None:
-        await message.answer("Достигнуто максимальное количество медиафайлов.", reply_markup=button)
+                                    else:
+                                        if item.photo_9 != None:
+                                            await message.answer("Достигнуто максимальное количество медиафайлов.", reply_markup=button)
 
     await message.answer_video(video=video, caption=("Видео добавлено.\n"
                 "Название: {name}"
